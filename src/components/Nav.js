e28b0969380f2f2fcd93e507129196eeaa83ab66
Nishar <neesharansari12341@gmail.com>
2024-08-03 18:02:25 +0530

    add content in nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import {Link,useNavigate} from 'react-router-dom';
-
+// add something new here
 const Nav =()=>{
   const auth =localStorage.getItem('user');
   const navigate =useNavigate();
